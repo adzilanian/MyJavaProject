224cc553b35de1c4ed03a8e683363342850a450e
adzilanian <dzilanian.andrei@yandex.ru>
2019-08-03 11:13:09 +0200

    refactor of tasks

--- src/chapter_001/src/main/java/ru/myjava/array/Check.java
+++ src/chapter_001/src/main/java/ru/myjava/array/Check.java
@@ -3,16 +3,12 @@ package chapter_001.src.main.java.ru.myjava.array;
 public class Check {
 
     public boolean mono(boolean[] data) {
-        boolean result = false;
         int counterOfTrue = 0;
         for (boolean dataCheckOfTrue : data) {
             if (dataCheckOfTrue) {
                 counterOfTrue++;
             }
-            if (counterOfTrue == data.length || counterOfTrue == 0) {
-                result = true;
-            }
         }
-            return result;
+        return counterOfTrue == data.length || counterOfTrue == 0;
     }
 }
